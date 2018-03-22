b961342f4fd53eccd1a3ffc4089998361592b827
alexandre-goux <alexandre.goux.44@gmail.com>
2018-03-22 11:23:51 +0100

    debut exercice 4 avec MailTest done

--- ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailTest.java
+++ ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailTest.java
@@ -1,5 +1,7 @@
 package com.acme.mailreader.domain;
 
+import java.time.Instant;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -7,12 +9,20 @@ import com.acme.mailreader.utils.DateIncorrecteException;
 
 public class MailTest {
 	
-
-	@Ignore
 	@Test(expected=DateIncorrecteException.class)
-	public final void erreurSiDateAvant1979() throws DateIncorrecteException {
-		//TODO
-				
+	public final void erreurSiDateAvant1970() throws DateIncorrecteException {
+		Mail mail1 = new Mail();
+		mail1.setDate(Instant.parse("1968-01-01T00:00:00.00Z"));
+		
 	}
+	
+	@Test(expected=DateIncorrecteException.class)
+	public final void erreurSiDateApres2100() throws DateIncorrecteException {
+		Mail mail1 = new Mail();
+		mail1.setDate(Instant.parse("2101-01-01T00:00:00.00Z"));
+		
+	}
+	
+	
 
 }
