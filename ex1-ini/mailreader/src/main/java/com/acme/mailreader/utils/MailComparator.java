cc8373be1bd3368e8a42123ac20c31a91304c3ff
martin <martin@accolad.fr>
2018-03-22 10:19:05 +0100

    Update Ex1

--- ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
+++ ex1-ini/mailreader/src/main/java/com/acme/mailreader/utils/MailComparator.java
@@ -12,27 +12,57 @@ import com.acme.mailreader.model.Mail;
  */
 public class MailComparator implements Comparator<Mail> {
 
-	public int compare(Mail obj1, Mail obj2) {
-		if (obj1 == null || obj2 == null) {
+	public int compare(Mail mail1, Mail mail2) {
+		if (unDesMailsEstNul(mail1, mail2)) {
 			return 0;
 		}
-		if (obj1.isImportant() != obj2.isImportant()) {
-			if (obj1.isImportant() && !obj2.isImportant()) {
-				return -1;
-			} else {
-				return 1;
-			}
+		if (pasLaMemeImportance(mail1, mail2)) {
+			return trierParImportance(mail1, mail2);
 		}
-		if (obj1.getStatut() != obj2.getStatut()) {
-			int comp = obj1.getStatut().ordinal()
-					- obj2.getStatut().ordinal();
-			return comp > 0 ? -1 : 1;
+		if (pasLeMemeStatut(mail1, mail2)) {
+			return trierParStatut(mail1, mail2);
 		}
-		if (obj1.getSujet() != obj2.getSujet()) {
-			return obj2.getSujet().compareTo(obj1.getSujet());
+		if (pasLeMemeSujet(mail1, mail2)) {
+			return trierParSujet(mail1, mail2);
 		}
-		return obj2.getDate().compareTo(obj1.getDate());
+		return trierParDate(mail1, mail2);
 	}
-	
 
+	private int trierParDate(Mail mail1, Mail mail2) {
+		return mail2.getDate().compareTo(mail1.getDate());
+	}
+
+	private int trierParSujet(Mail mail1, Mail mail2) {
+		return mail2.getSujet().compareTo(mail1.getSujet());
+	}
+
+	private boolean pasLeMemeSujet(Mail mail1, Mail mail2) {
+		return mail1.getSujet() != mail2.getSujet();
+	}
+
+	private int trierParStatut(Mail mail1, Mail mail2) {
+		int comp = mail1.getStatut().ordinal()
+				- mail2.getStatut().ordinal();
+		return comp > 0 ? -1 : 1;
+	}
+
+	private boolean pasLeMemeStatut(Mail mail1, Mail mail2) {
+		return mail1.getStatut() != mail2.getStatut();
+	}
+
+	private int trierParImportance(Mail mail1, Mail mail2) {
+		if (mail1.isImportant() && !mail2.isImportant()) {
+			return -1;
+		} else {
+			return 1;
+		}
+	}
+
+	private boolean pasLaMemeImportance(Mail obj1, Mail obj2) {
+		return obj1.isImportant() != obj2.isImportant();
+	}
+
+	private boolean unDesMailsEstNul(Mail obj1, Mail obj2) {
+		return obj1 == null || obj2 == null;
+	}
 }
