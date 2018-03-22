ce5b1a1b7b58fc4a24f359a80b33d8fc61d980a0
martin <martin@accolad.fr>
2018-03-22 11:48:25 +0100

    Add MailComparator tests

--- ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailComparatorTest.java
+++ ex3-ini/mailreader/src/test/java/com/acme/mailreader/domain/MailComparatorTest.java
@@ -1,8 +1,13 @@
 package com.acme.mailreader.domain;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.*;
+
+
 import static org.junit.Assert.assertThat;
 
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsNot;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,19 +24,23 @@ public class MailComparatorTest {
 	public final void egauxSiDeuxMailsNuls() {
 		Mail mail1 = null;
 		Mail mail2 = null;
-		assertThat(comparator.compare(mail1, mail2), is(0));
+		assertThat(comparator.compare(mail1, mail2), is(MailComparator.EGAUX));
+	}
+	
+	//Autres tests unitaires
+	
+	@Test
+	public final void deuxiemeMailPlusImportant() {
+		Mail mail1 = new Mail.Builder("sujet").important(false).build();
+		Mail mail2 = new Mail.Builder("sujet").important(true).build();
+		assertThat(comparator.compare(mail1, mail2), is(MailComparator.PREMIER_PLUS_PETIT));
 	}
 	
 	@Test
-	public final void egauxSiUnDesMailsNuls() {
-		Mail mail1 = new Mail();
-		Mail mail2 = null;
-		assertThat(comparator.compare(mail1, mail2), is(0));
+	public final void sujetsNeSontPasEgaux() {
+		Mail mail1 = new Mail.Builder("sujet1").build();
+		Mail mail2 = new Mail.Builder("sujet2").build();
+		assertThat(comparator.compare(mail1, mail2), is(not(MailComparator.EGAUX)));
 	}
-	
-	//TODO
-	//Autres tests unitaires
-	
-	
 	
 }
